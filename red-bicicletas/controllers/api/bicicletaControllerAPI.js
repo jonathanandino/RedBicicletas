f41f77339734014598f3d2104a06d576ba94dbaa
jonathanandino <jonathanandinoperez@gmail.com>
2023-03-25 20:35:06 -0300

    Red Bicicletas

---
+++ red-bicicletas/controllers/api/bicicletaControllerAPI.js
@@ -0,0 +1,23 @@
+var Bicicleta = require('../../models/bicicleta');
+
+exports.bicicleta_list = function(req, res){
+    res.status(200).json({
+        bicicletas: Bicicleta.allBicis
+    });
+}
+
+exports.bicicletas_create = function(req, res) {
+    var bici = new Bicicleta(req.body.id, req.body.color, req.body.model);
+    bici.ubicacion = [req.body.lat, req.body.lng];
+
+    Bicicleta.add(bici);
+
+    res.status(200).json({
+        bicicleta:bici
+    });
+}
+
+exports.bicicletas_delete = function(req, res) {
+    Bicicleta.removeById(req.body.id);
+    res.status(204).send();
+}
---
+++ red-bicicletas/public/javascripts/map.js
@@ -0,0 +1,20 @@
+var map = L.map('main_map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+//L.marker([51.5, -0.09]).addTo(map);
+
+
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici) {
+           L.marker(bici.ubicacion, {title: bici.id} ).addTo(map);
+        });
+    }
+})
